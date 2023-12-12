c202b49a8015f3fcc94e3ea3cd792a13885416de
hashir4560 <haashirraja@gmail.com>
2023-12-12 17:27:33 +0500

    Made home component and made changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,17 @@ import "./App.css";
 
 //components
 import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+
+import { Box } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
+      <Box style={{ marginTop: 54 }}>
+        <Home />
+      </Box>
     </div>
   );
 }
