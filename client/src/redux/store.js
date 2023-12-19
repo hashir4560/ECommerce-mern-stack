cdf22361479924bbde546a74c4860a413df69f67
hashir4560 <haashirraja@gmail.com>
2023-12-19 12:58:48 +0500

    Resolved dependencies Issues and Update

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,9 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
-
-import thunk from "redux-thunk";
+import thunk from "redux-thunk"; // Update import statement
 import { composeWithDevTools } from "@redux-devtools/extension";
+
+//reducers
+import { getProductsReducer } from "./reducers/productReducer";
 
 const reducer = combineReducers({
   getProducts: getProductsReducer,
