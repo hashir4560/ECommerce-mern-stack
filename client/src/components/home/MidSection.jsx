6ccc819fd8603c344c6eecd794f6d4a007db12d5
hashir4560 <haashirraja@gmail.com>
2023-12-22 10:17:40 +0500

    Update

--- client/src/components/home/MidSection.jsx
+++ client/src/components/home/MidSection.jsx
@@ -1,10 +1,14 @@
-import {  Grid } from "@mui/material"
+import {  Grid ,styled} from "@mui/material"
 import { imageURL } from "../../constants/data"
 
 
+const Wrapper=styled(Grid)`
+    margin-top:20px;
+`
+
 const MidSection=()=>{
     return(
-        <Grid lg={12} sm={12} md={12} xs={12} container >
+        <Wrapper lg={12} sm={12} md={12} xs={12} container >
             {
                 imageURL.map(image=>(
                     <Grid item lg={4} md={4} sm={12} xs={12}>
@@ -12,8 +16,7 @@ const MidSection=()=>{
                     </Grid>
                 ))  
             }
-        </Grid>
-
+       </Wrapper>
     )
 
 }
