563e521233afeb569d0419271c9c09cbe6993361
hashir4560 <haashirraja@gmail.com>
2023-12-26 16:59:55 +0500

    Added styling

--- client/src/components/details/DetailView.jsx
+++ client/src/components/details/DetailView.jsx
@@ -2,13 +2,34 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getProductDetails } from "../../redux/actions/productActions.js";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography,styled,Grid } from "@mui/material";
 import ActionItem from "./ActionItem.jsx";
+
+const Component=styled(Box)`
+    background:#F2F2F2;
+    margin-top:55px;
+
+`;
+
+const Container=styled(Grid)`
+    background:#FFFFFF;
+    display:flex;
+`
+
+const RightContainer=styled(Grid)`
+    margin-top:50px;
+
+
+`
+
 
 const DetailView = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
   const { loading, product } = useSelector((state) => state.getProductDetails);
+
+  const fassured = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/fa_62673a.png'
+
 
   useEffect(() => {
     // Fetch product details only if the id is different or product is not available
@@ -20,19 +41,28 @@ const DetailView = () => {
   console.log(product);
 
   return (
-    <Box>
+    <Component>
       {product && Object.keys(product).length && (
-        <Box>
-          <Box>
+        <Container container>
+          <Grid item lg={4} md={4} sm={8} xs={12}>
             <ActionItem product={product}/>
-          </Box>
-          <Box>
-            {/* Make sure the structure of product.title and product.title.longTitle is correct */}
+          </Grid>
+          <RightContainer item  lg={8} md={8} sm={8} xs={12}>
             <Typography>{product.title.longTitle}</Typography>
-          </Box>
-        </Box>
+            <Typography style={{marginTop:5,color:'#878787',fontSize:14}}>8 Ratings & 1 Reviews 
+            <Box component='span'><img src={fassured} alt="" style={{width:77,marginLeft:20}}/></Box>
+            </Typography>
+            <Typography>
+                <Box component='span' style={{fontSize:28}}>{product.price.cost}</Box>&nbsp;&nbsp;&nbsp;
+                <Box component='span' style={{color:"#878787"}}><strike>{product.price.mrp}</strike></Box>&nbsp;&nbsp;&nbsp;
+                <Box component='span' style={{color:"#388E3C"}}>{product.price.discount}</Box>&nbsp;&nbsp;&nbsp;
+
+                
+            </Typography>
+          </RightContainer>
+        </Container>
       )}
-    </Box>
+    </Component>
   );
 };
 
