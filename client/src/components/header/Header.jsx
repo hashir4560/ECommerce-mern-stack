979d571994d146a8a27e40bbd319166c6ff62d55
hashir4560 <haashirraja@gmail.com>
2023-12-28 16:19:57 +0500

    made Header responsive

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 
+import { useState } from 'react';
 
-import {AppBar,Toolbar,Box,styled,Typography} from '@mui/material';
+import {AppBar,Toolbar,Box,styled,Typography,Drawer, IconButton, List,ListItem} from '@mui/material';
+import{  Menu} from '@mui/icons-material';
 
 //components
 import Search from './Search';
@@ -31,9 +33,22 @@ const PlusImage=styled('img')({
 
 })
 
-const CustomButtonWrapper=styled(Box)`
-    margin: 0 5% 0 auto;
-`
+const CustomButtonWrapper=styled(Box)(({theme})=>({
+    margin:'0 5% 0 auto',
+    [theme.breakpoints.down('md')]:{
+        display:'none',
+    }
+
+}));
+
+const MenuButton=styled(IconButton)(({theme})=>({
+    display:'none',
+    [theme.breakpoints.down('md')]:{
+        display:'block',
+    }
+
+}))
+
 
 
 
@@ -41,9 +56,42 @@ const Header=()=>{
     const logoURL='https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png';
     const subURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png';
 
+    const [open,setOpen]=useState(false)
+    
+    const handleOpen=()=>{
+        setOpen(true);
+
+    }
+    
+    const handleClose=()=>{
+        setOpen(false);
+    }
+
+    const list=()=>(
+        <Box style={{width:200}} onClick={handleClose}>
+            <List>
+            <ListItem button >
+                <CustomButtons/>
+
+            </ListItem>
+            </List>
+        </Box>
+    )
+
+
+
     return(
    <StyledHeader>
       <Toolbar style={{minHeight:55}}>
+        <MenuButton color='inherit' onClick={handleOpen}>
+           <Menu/>
+        </MenuButton>
+
+        <Drawer open={open} onClose={handleClose}>
+            {list()}
+
+           
+        </Drawer>
         <Component to ="/">
             <img src={logoURL} alt='logo' style={{width:75}}/>
             <Box style={{display:'flex'}}>
@@ -52,6 +100,7 @@ const Header=()=>{
                   <Box component='span' style={{color: '#FFE500'}}>Plus</Box>
                         </SubHeading>
                         <PlusImage src={subURL} alt='sub-logo'/>
+
             </Box>
             </Component>
             <Search/>
