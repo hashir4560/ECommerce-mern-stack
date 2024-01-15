e99d66da7bcddac16c7376414bbdd000d1787411
hashir4560 <haashirraja@gmail.com>
2024-01-15 17:12:28 +0500

    Added Ellipsis for long text and updated

--- client/src/components/cart/CartItem.jsx
+++ client/src/components/cart/CartItem.jsx
@@ -1,4 +1,6 @@
-import { Box, Typography, styled } from "@mui/material";
+
+import { Box, Typography, styled,Button } from "@mui/material";
+import { addEllipsis } from "../../utils/common-utils";
 
 const Component = styled(Box)`
   border-top: 1px solid #f0f0f0;
@@ -20,6 +22,14 @@ const FAssuredImage = styled("img")`
   margin-left: 10px;
 `;
 
+const Remove=styled(Button)`
+    margin-top:20px;
+    font-size:16px;
+    color:#000;
+    font-weight:600;
+    
+`
+
 const CartItem = ({ item }) => {
   const fassured =
     "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/fa_62673a.png";
@@ -29,10 +39,22 @@ const CartItem = ({ item }) => {
       <LeftComponent>
         <img src={item.url} alt="product" />
       </LeftComponent>
-      <Box>
-        <Typography>{item.title.longTitle}</Typography>
-        <SmallText>Seller:RetailNet</SmallText>
-            <Box component="span"><FAssuredImage src={fassured} alt="flipkart"  /></Box>
+      <Box style={{margin:20}}>
+        <Typography>{addEllipsis(item.title.longTitle)}</Typography>
+        <SmallText>Seller:RetailNet
+         <Box component="span"><FAssuredImage src={fassured} alt="flipkart"  /></Box>
+        </SmallText>
+        <Typography style={{margin:'20px 0'}}>
+            <Box component='span' style={{fontWeight:600,fontSize:16}}>Rs {item.price.cost}</Box>&nbsp;&nbsp;&nbsp;
+            <Box component='span' style={{color:"#878787"}}><strike>Rs{item.price.mrp}</strike></Box>&nbsp;&nbsp;&nbsp;
+            <Box component='span' style={{color:"#388E3C"}}>{item.price.discount}Off</Box>                
+            </Typography>
+          
+        <Remove>
+            Remove 
+        </Remove>
+            
+
        
       </Box>
     </Component>
