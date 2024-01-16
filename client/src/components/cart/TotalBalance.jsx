21c3497d99fbfa53b29497d68a824877eb2d6836
hashir4560 <haashirraja@gmail.com>
2024-01-16 18:48:10 +0500

    Update

--- client/src/components/cart/TotalBalance.jsx
+++ client/src/components/cart/TotalBalance.jsx
@@ -1,3 +1,4 @@
+import { useState ,useEffect} from "react";
 import styled from "@emotion/styled"
 import {Box, Typography } from "@mui/material"
 
@@ -33,32 +34,50 @@ const Price=styled(Box)`
 
 const Discount=styled(Typography)`
     color:green ;
-    font-weight:600;
+    font-weight:500;
 `
 
-const TotalBalance=({CartItems})=>{
+const TotalBalance=({cartItems})=>{
+
+    const [price, setPrice] = useState(0);
+    const [discount, setDiscount] = useState(0)
+
+    useEffect(() => {
+        totalAmount();
+    }, [cartItems]);
+    
+    const totalAmount = () => {
+        let price = 0, discount = 0;
+        cartItems.map(item => {
+            price += item.price.mrp
+            discount += (item.price.mrp - item.price.cost) 
+        })
+        setPrice(price);
+        setDiscount(discount);
+    }
+
     return (
        <Box>
         <Header>
             <Heading>PRICE DETAILS</Heading>
         </Header>
         <Container>
-            <Typography>Price ({CartItems?.length}item)
-                <Price component='span'>Rs 100</Price>
+            <Typography>Price ({cartItems?.length}item)
+                <Price component='span'>Rs{price}</Price>
             
             </Typography>
             <Typography>Discount
-                <Price component='span'>-Rs 100</Price>
+                <Price component='span'>Rs{discount}</Price>
             
             </Typography>
             <Typography>Delivery 
-                <Price component='span'>Rs 100</Price>
+                <Price component='span'>Rs100</Price>
             
             </Typography>
             <Typography variant="h6">Total Amount
-                <Price  component='span'>Rs 100</Price>
+                <Price  component='span'>Rs{price-discount+100}</Price>
             </Typography>
-            <Discount> You will save Rs 100 on this Order</Discount>
+            <Discount> You will save Rs{discount-100}on this Order</Discount>
         </Container>
        </Box>
     )
